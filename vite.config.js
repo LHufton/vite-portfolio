2e9a85de0999c56d8fa3d20ef06be97840d16809
LHufton <lukehufton@gmail.com>
2023-09-09 03:01:10 -0500

    build bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy' // Import the legacy plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), legacy()], // Add the legacy plugin
+  build: {
+    target: 'es2015', // Set the target to es2015
+    outDir: 'docs' // Set the output directory to docs instead of dist
+  }
 })
